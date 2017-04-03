c2935100629f98178273b76e37837690fa738a36
vol4tim <vol4tim@gmail.com>
2017-04-03 10:14:04 +0400

    add env gas

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,8 @@ const API_URL = process.env.API_URL || 'http://localhost:3000';
 const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
 const FROM_ADDRESS = process.env.FROM_ADDRESS || '';
 const CHAINID = process.env.CHAINID || 42; // kovan
+const GAS_PRICE = process.env.GAS_PRICE || 20000000000;
+const GAS_LIMIT = process.env.GAS_LIMIT || 70000;
 
 export const sign = function(tokenAddress, amount, complierAddress) {
   if (FROM_ADDRESS == '' || PRIVATE_KEY == '') {
@@ -20,8 +22,8 @@ export const sign = function(tokenAddress, amount, complierAddress) {
       const privateKey = Buffer.from(PRIVATE_KEY, 'hex');
       const txParams = {
         nonce: '0x'+ Number(count).toString(16),
-        gasPrice: 20000000000,
-        gasLimit: 60000,
+        gasPrice: GAS_PRICE,
+        gasLimit: GAS_LIMIT,
         from: FROM_ADDRESS,
         to: complierAddress,
         data: '0x9dc29fac000000000000000000000000'+ tokenAddress.replace(/^0x/, '') + _.padStart(Number(amount).toString(16), 64, '0'),
